387caad19892c955fe3f71485e31fe2db88efab7
Bao Huynh <baohuynh@hoiio.com>
2023-06-27 11:48:11 +0700

    remove mobile

--- package/example/android/app/src/main/java/com/zezo357/flutter_meedu_videoplayer_example/MainActivity.java
+++
@@ -1,8 +0,0 @@
-package com.zezo357.flutter_meedu_videoplayer_example;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-import com.zezo357.flutter_meedu_videoplayer.MeeduPlayerFlutterActivity;
-
-public class MainActivity  extends MeeduPlayerFlutterActivity {
-}
